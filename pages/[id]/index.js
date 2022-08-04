9422d74a42a6647a7befd24d840511b5c3c91193
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-08-04 19:35:10 +0500

    Displaying Each Dynamic Page

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,45 +1,50 @@
-import React from 'react'
+import React from "react";
 
-export let getStaticPaths=async ()=>
-{
-    let Res=await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    let Data=await Res.json();
+export let getStaticPaths = async () => {
+  let Res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  let Data = await Res.json();
 
-    let AllPages=Data.map((El)=>
-    {
-        return{
-            params:{
-                id:El.id.toString()
-            }
-        }
-    })
+  let AllPages = Data.map((El) => {
+    return {
+      params: {
+        id: El.id.toString(),
+      },
+    };
+  });
 
-    return{
-        paths:[...AllPages],
-        fallback:'blocking',
-    }
-}
+  return {
+    paths: [...AllPages],
+    fallback: "blocking",
+  };
+};
 
-export let getStaticProps=async (context)=>
-{
-    let ID=context.params.id
-    let Res=await fetch(`https://jsonplaceholder.typicode.com/posts/${ID}`);
-    let Data=await Res.json();
+export let getStaticProps = async (context) => {
+  let ID = context.params.id;
+  let Res = await fetch(`https://jsonplaceholder.typicode.com/posts/${ID}`);
+  let Data = await Res.json();
 
-    return{
-        props:{
-            Data
-        }
-    }
-}
-const Pages = ({Data}) => {
+  return {
+    props: {
+      Data,
+    },
+  };
+};
+const Pages = ({ Data }) => {
   return (
-    <div>
-        {
-            
-        }
-    </div>
-  )
-}
 
-export default Pages
+    <div className="flex justify-center">
+        <div className="max-w-xl border-2 mt-5 p-3 space-y-4 border-yellow-300 m-2 text-sm md:text-lg">
+        <span className="flex items-center justify-center text-center bg-yellow-300  object-cover h-[30px] w-[30px] text-yellow-800 rounded-full">1</span>
+
+        <p className="capitalize border-b-4 border-green-500 w-fit">
+            {Data.title}
+        </p>
+        <p className="capitalize">
+            {Data.body}
+        </p>
+        </div>
+    </div>
+  );
+};
+
+export default Pages;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,73 @@
 import Link from "next/link";
-export default function Home({Data}) {
-  return <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-    {
-      Data.map((Elem)=>
-      {
-        return(
-          <Link href={`/${Elem.id}`}>
-            <div className="border-2 m-2 p-3 border-l-teal-500 flex justify-between flex-col cursor-pointer">
-                <span className="flex items-center justify-center text-center bg-yellow-300  object-cover h-[30px] w-[30px] text-yellow-800 rounded-full">{Elem.id}</span>
-                <p>{Elem.title}</p>
-            </div>
-          </Link>
-        )
-      })
-    }
-  </div>;
+import { useEffect, useState } from "react";
+
+export default function Home({ Data }) {
+  let [State, setState] = useState(0);
+  let Dec = () => {
+
+    State>0?setState(State - 1):setState(0);
+  };
+
+  let Inc = () => {
+
+    (State*6)+6 < Data.length ?setState(State + 1):setState(State);
+  };
+  return (
+    <div>
+      <div
+        className="flex rounded-md justify-center items-center my-5 focus:border-none"
+        role="group">
+        <button
+          onClick={() => {
+            Dec();
+          }}
+          type="button"
+          className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        >
+          -
+        </button>
+        <button
+          type="button"
+          className="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        >
+          {State}
+        </button>
+        <button
+          onClick={() => {
+            Inc();
+          }}
+          type="button"
+          className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        >
+          +
+        </button>
+
+      </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          {Data.slice(State * 6, State * 6 + 6).map((Elem) => {
+            return (
+              <Link href={`/${Elem.id}`} key={Elem.id}>
+                <div className="border-2 m-2 p-3 border-l-teal-500 flex justify-between flex-col cursor-pointer">
+                  <span className="flex items-center justify-center text-center bg-yellow-300  object-cover h-[30px] w-[30px] text-yellow-800 rounded-full">
+                    {Elem.id}
+                  </span>
+                  <p>{Elem.title}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+    </div>
+  );
 }
 
-export let getStaticProps=async ()=>
-{
-  let Res=await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  let Data=await Res.json();
+export let getStaticProps = async () => {
+  let Res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  let Data = await Res.json();
 
   return {
     props: {
       Data,
-    }
-  }
-}
+    },
+  };
+};
